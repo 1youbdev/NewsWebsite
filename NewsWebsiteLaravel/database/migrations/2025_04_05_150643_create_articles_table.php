db82dbc609a42f77076d1284c5e3aa59f16d809f
Rizki <Rizki@gmail.com>
2025-04-07 02:21:01 +0100

    hopefully the solution

--- NewsWebsiteLaravel/database/migrations/2025_04_05_150643_create_articles_table.php
+++ NewsWebsiteLaravel/database/migrations/2025_04_05_150643_create_articles_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-        $table->enum('role', ['admin', 'auteur','utilisateur'])->default('utilisateur');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('idUser')->constrained('users')->OnDelete('cascade');
+            $table->foreignId('idCategory')->constrained('categories')->OnDelete('cascade');
+            $table->string('title');
+            $table->text('content');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
     }
 };
